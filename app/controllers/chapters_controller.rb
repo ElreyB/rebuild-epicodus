ac5ae4b8747b45b72484b4f01eea7d3a24c26bee
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-14 11:27:01 -0800

    add chapters to app

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,4 +2,28 @@ class ChaptersController < ApplicationController
   def index
     @chapters = Chapter.all
   end
+
+  def show
+    @chapter = Chapter.find(params[:id])
+  end
+
+  def new
+    @chapter = Chapter.new
+  end
+
+  def create
+    @chapter = Chapter.new(chapter_params)
+    if @chapter.save
+      flash[:notice] = "Your chapter has been saved!"
+      redirect_to chapters_path
+    else
+      render new
+    end
+  end
+
+private
+  def chapter_params
+    params.require(:chapter).permit(:name)
+  end
+
 end
