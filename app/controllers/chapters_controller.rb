ca85d697cc8636a7b9b55e0aedde9316921c1b8d
Elrey Belmonti <elbelmonti@gmail.com>
2017-11-14 12:36:59 -0800

    add full CRUD for chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -21,6 +21,25 @@ class ChaptersController < ApplicationController
     end
   end
 
+  def edit
+    @chapter = Chapter.find(params[:id])
+  end
+
+  def update
+    @chapter = Chapter.find(params[:id])
+    if @chapter.update(chapter_params)
+      redirect_to chapters_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @chapter = Chapter.find(params[:id])
+    @chapter.destroy
+    redirect_to chapters_path
+  end
+
 private
   def chapter_params
     params.require(:chapter).permit(:name)
